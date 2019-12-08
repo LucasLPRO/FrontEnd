2d2e4b2aa2df30b7135106e15a5a42ab727f8ecb
Lucas <Lucas@192.168.1.48>
2019-12-08 12:34:34 +0100

    Administration du tableau js

--- TP3-JS/TP3 - projet/js/animaux.js
+++ TP3-JS/TP3 - projet/js/animaux.js
@@ -1,11 +1,13 @@
 window.addEventListener("load", genererTableauAnimaux);
+window.addEventListener("load", adminTableau);
+
 
 function genererTableauAnimaux() {
     var table = document.getElementById("tableauAnimaux");
     var i;
-    var j;
     for (i = 0; i < jsonAnimaux.Animal.length; i++) {
         var tr = document.createElement('tr');
+        tr.id = "ligne" + i;
         tr.className = "ligneTableau";
         table.appendChild(tr);
         //Nom
@@ -34,13 +36,47 @@ function genererTableauAnimaux() {
         tr.appendChild(tdPhoto);
         tdPhoto.appendChild(imgPhoto);
         
-        //Bouton
+        //Boutons
         var tdButton = document.createElement('button');
         tdButton.type= "button";
-        tdButton.name = "button";
-        tdButton.className = "button";
         var tdButtonTexte = document.createTextNode("Découvrir");
         tr.appendChild(tdButton);
-        tdButton.appendChild(tdButtonTexte);       
+        tdButton.appendChild(tdButtonTexte);
+        
+        var tdBtnSuppr = document.createElement('button');
+        tdBtnSuppr.type = "button";
+        tdBtnSuppr.id = "btnSuppr";
+        var tdBtnSuppTxt = document.createTextNode("Supprimer");
+        tr.appendChild(tdBtnSuppr);
+        tdBtnSuppr.appendChild(tdBtnSuppTxt);
+       //tdBtnSuppr.onclick = onClickBtSuppr();
+       //tdBtnSuppr.onclick = "this.parentNode.parentNode.parentNode.deleteRow(this.parentNode.parentNode.rowIndex)";
     }
+}
+
+function adminTableau() {
+   // document.getElementById("btnSuppr").addEventListener("click", onClickBtSuppr(document.getElementById("montableau" ).rowIndex));
+  document.getElementById("btnSuppr").addEventListener("click", onClickBtSuppr);
+}
+
+function onClickBtSuppr() {
+//    document.getElementById("tableauAnimaux").deleteRow(3);
+//    window.alert("click");
+
+//  document.getElementById('tableau').deleteRow(ligne.rowIndex);
+// 
+//  var tableau = document.getElementById('tableau');
+//  var trs = tableau.rows;
+//  var n = trs.length;
+//  var i;
+// 
+//  alert(n);
+//  for (i = 1; i < n; i++)
+//  {
+//    trs[i].cells[0].innerHTML = trs[i].rowIndex;
+//  }
+    $("table").find("#remove").on ("click", function (){
+$(this).closest('tr').remove();
+});
+
 }
--- TP3-JS/TP3 - projet/js/model.js
+++ TP3-JS/TP3 - projet/js/model.js
@@ -1,7 +1,7 @@
 var texteAnimaux = '{ "Animal":[' +
 '{ "nom":"Phanphan" , "description":"Le seul éléphant rose du monde.", "pays":"Chine", "photo":"../images/elephant.jpeg" },' +
 '{ "nom":"Orquie" , "description":"Le plus petit orque du monde.", "pays":"Monaco", "photo":"../images/orque.jpeg" },' +
-'{ "nom":"Boobie" , "description":"Le plus petit orque du monde.", "pays":"Monaco", "photo":"../images/ours.jpeg" },' +
+'{ "nom":"Boobie" , "description":"Le plus petit ours du monde.", "pays":"Monaco", "photo":"../images/ours.jpeg" },' +
 '{ "nom":"Biquette" , "description":"La chèvre la plus belle du monde.", "pays":"France", "photo":"../images/biquette.jpg" },' +
 '{ "nom":"Marie" , "description":"Le chat le plus distingué du monde.", "pays":"France", "photo":"../images/chat.jpg" },' +
 '{ "nom":"Piou" , "description":"Le poussin le plus gros au monde.", "pays":"Monaco", "photo":"../images/piou.jpeg" },' +
